b0b10e1f4ee4c820b26f8ba66d54a5fa4dd4a3e1
Gordon Doskas <gordon2012@users.noreply.github.com>
2016-08-15 20:04:17 -0700

    Add smooth scrolling to navigation links

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,5 +1,16 @@
 $(function(){
-    $('.test-button').click(function(){
-        alert('CLICKED!');
+
+    // Smooth scrolling
+    var DURATION = 300;
+
+    $('a[href^="#"]').click(function(event){
+        var target = $($(this).attr('href'));
+
+        if(target.length){
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: target.offset().top
+            }, DURATION);
+        }
     });
 });
